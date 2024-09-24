c79e6bd4c3f0c842e7a8f19d335c8676b27c7283
Away <isalexpro1615@qq.com>
2024-09-24 22:30:32 +0800

    Site updated: 2024-09-24 22:30:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/9b8f8987.html","posts/fe7ff2b6.html","posts/5feda8e2.html","posts/7b1a3fe4.html"];function toRandomPost(){
+var posts=["posts/9b8f8987.html","posts/7e158fb0.html","posts/fe7ff2b6.html","posts/5feda8e2.html","posts/7b1a3fe4.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
